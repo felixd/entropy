133fee18271928108a94134abc30c49d2b95659d
Paweł 'felixd' Wojciechowski <felixd@konopnickiej.com>
2023-02-27 12:21:00 +0100

    Some more

--- go/entropy.go
+++ go/entropy.go
@@ -3,9 +3,22 @@ package main
 import (
 	"crypto/rand"
 	"encoding/binary"
-	"os"
 	"flag"
+	"fmt"
+	"os"
 )
+
+func uniqueBytes(byteSlice []byte) []byte {
+	keys := make(map[byte]bool)
+	list := []byte{}
+	for _, entry := range byteSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
 
 func main() {
 
@@ -19,6 +32,19 @@ func main() {
 	if _, err := rand.Read(randBytes); err != nil {
 		panic(err)
 	}
+
+	uniqueVals := uniqueBytes(randBytes)
+
+	fmt.Println("uniqueBytes/ randBytes:")
+	fmt.Println(uniqueVals)
+	fmt.Println(randBytes)
+
+	numUniqueVals := len(uniqueVals)
+	// Calculate the number of bits of entropy
+	numBitsOfEntropy := numUniqueVals * 8
+
+	// Simple Entropy check:
+	fmt.Printf("Number of bits of entropy (unique Bytes) in the []byte slice: %d\n", numBitsOfEntropy)
 
 	// Convert the slice of random numbers to a binary file
 	f, err := os.Create("random.bin")
